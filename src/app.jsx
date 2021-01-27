e5587f91bca614e413d0d6401ffa94ece49f1eac
HJtime <hj_time@naver.com>
2021-01-27 19:18:12 +0900

    add login functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,20 @@
 import './app.css';
 import Login from './componentes/login/login';
+import Maker from './componentes/maker/maker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App({authService}) {
   return (
-    <Login authService={authService}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={['/', '/Address-book']}>
+          <Login authService={authService}/>
+        </Route>
+        <Route path="/maker">
+          <Maker/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/componentes/login/login.jsx
+++ src/componentes/login/login.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from './login.module.css';
 
 const Login = ({authService}) => {
+    const history=useHistory();
+
+    const goToMaker=(userId)=>{
+        history.push({
+            pathname: '/maker',
+            state:{id: userId},
+        });
+    }
+
     const onLogin=(event)=>{
-        authService.login(event.currentTarget.textContent);
+        authService
+        .login(event.currentTarget.textContent)
+        .then(data=>goToMaker(data.user.uid));
     };
+
+    useEffect(()=>{
+        authService.onAuthChange(user=>{
+            user&&goToMaker(user.uid);
+        })
+    })
     return(
         <section className={styles.section}>
             <div className={styles.container}>
